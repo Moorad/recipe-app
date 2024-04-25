d6df6a5dfc1e764c9d52d1a8a2b60d766c6ba19f
Moorad <moorad2003@gmail.com>
2024-04-25 11:57:01 +0300

    Improve UI and fix bug where recipe details crash after webpage refresh

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,12 +3,19 @@ import { ResolveFn } from '@angular/router';
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
 import { RecipeService } from './recipe.service';
+import { tap } from 'rxjs';
 
 export const recipesResolver: ResolveFn<Recipe[]> = () => {
   const recipeService = inject(RecipeService);
 
   if (recipeService.recipes.length <= 0) {
-    return inject(DataStorageService).fetchRecipes();
+    return inject(DataStorageService)
+      .fetchRecipes()
+      .pipe(
+        tap((recipes) => {
+          recipeService.setRecipes(recipes);
+        })
+      );
   } else {
     return recipeService.recipes;
   }
